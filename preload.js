9922ad6107f849b41452ca26011d6ba168dc61de
Komoszek <komoszek10@gmail.com>
2020-10-07 09:56:39 +0200

    Remove unnecessary files

--- preload.js
+++
@@ -1,5 +0,0 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.fs = require('fs');
-window.ini = require('ini');
-window.grabber = require('./build/Release/grabber');
